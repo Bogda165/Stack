3b8df37aed2c0bf8988f2b43b0b48f5b31505542
Bohdan <koval.b@icloud.com>
2023-09-01 17:21:57 +0200

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Богдан Коваль on 01.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
